5888db1b09b9707d2ffb8f42cf9a7e0cd0395002
Nike Okoronkwo <nikechukwu@gmail.com>
2025-02-02 01:59:34 -0600

    Completed Layouts
    - Put Handwriting View on Pause due to Compatibility issues and I needed it to load
    - Done layout for other pages
    - Custom triangular layout for learning page
    - Added helpers as well with TODOs
    
    TODO:
    - Data Structures
    - Locations for Data Structures

---
+++ Views/Learn/LevelsView.swift
@@ -0,0 +1,18 @@
+//
+//  LevelsView.swift
+//  HandWriting
+//
+//  Created by Nikechukwu Okoronkwo on 02/02/2025.
+//
+
+import SwiftUI
+
+struct LevelsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LevelsView()
+}
